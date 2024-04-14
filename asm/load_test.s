4b8b1b647c11d95a79349ea8d42d74d6cca76f7f
Reezan Visram <reezan.visram@rogers.com>
2024-04-14 12:48:14 -0400

    clean

--- asm/load_test.s
+++ asm/load_test.s
@@ -4,3 +4,5 @@ _start:
   lw x2, 4(x0)
   lb x3, 4(x0)
   lh x4, 4(x0)
+  lbu x5, 4(x0)
+  lhu x6, 4(x0)
---
+++ asm/store_test.s
@@ -0,0 +1,8 @@
+  .global _start
+
+_start:
+  lw x2, 4(x0)
+
+  sb x2, 32(x0)
+  sh x2, 48(x0)
+  sw x2, 64(x0)
